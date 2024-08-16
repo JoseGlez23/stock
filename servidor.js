1cd34ea76a385ad25389f83ef380d587412c85ea
JoseGlez23 <josecmata234@gmail.com>
2024-08-15 17:51:53 -0700

    Actualizado nuevamente

--- servidor.js
+++ servidor.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const puerto = 3000;
@@ -10,18 +10,24 @@ app.use(cors());
 app.use(express.json());
 
 // Conexión a la base de datos
-mongoose.connect('mongodb+srv://mongoomongo465:xw5jacjU8kqFOUfR@cluster0.chcllyw.mongodb.net/inventario?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Conectado a MongoDB');
-}).catch(err => {
-  console.error('Error de conexión a la base de datos', err);
-});
+mongoose
+  .connect(
+    "mongodb+srv://josegm23c:eXif6tQ0679gsglI@cluster0.wgpmchs.mongodb.net/inventario",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Conectado a MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error de conexión a la base de datos", err);
+  });
 
 // Rutas
-const rutasComputadoras = require('./rutas/computadoraRutas');
-app.use('/api/computadoras', rutasComputadoras);
+const rutasComputadoras = require("./rutas/computadoraRutas");
+app.use("/api/computadoras", rutasComputadoras);
 
 // Iniciar el servidor
 app.listen(puerto, () => {
